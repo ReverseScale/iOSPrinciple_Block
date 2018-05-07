f3bae884dcb16f9d048fa753a3a3f035a7470284
Steven <reversescale@icloud.com>
2018-05-07 16:29:05 +0800

    🚀:Deploying stuff.

--- iOSPrinciple_Block/ViewController.m
+++ iOSPrinciple_Block/ViewController.m
@@ -17,12 +17,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self blockFunc1];
+    [self blockFunc2];
+    [self blockFunc3];
+    [self blockFunc4];
 }
+
+- (void)blockFunc1 {
+    int num = 100;
+    void (^block)() = ^{
+        NSLog(@"num equal %d", num);
+    };
+    num = 200;
+    block();
+}
+- (void)blockFunc2 {
+    __block int num = 100;
+    void (^block)() = ^{
+        NSLog(@"num equal %d", num);
+    };
+    num = 200;
+    block();
+}
+
+// 全局变量
+int num = 100;
+- (void)blockFunc3 {
+    void (^block)() = ^{
+        NSLog(@"num equal %d", num);
+    };
+    num = 200;
+    block();
+}
+
+- (void)blockFunc4 {
+    static int num = 100;
+    void (^block)() = ^{
+        NSLog(@"num equal %d", num);
+    };
+    num = 200;
+    block();
+}
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
-
 
 @end
